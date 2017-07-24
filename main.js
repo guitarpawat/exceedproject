411a8d58becd6c1848b1f58d8559db3157812163
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2017-07-24 12:11:58 +0700

    add some ajax

--- main.js
+++ main.js
@@ -13,58 +13,68 @@ $(document).ready(function() {
     );
   };
 
-  function get(key) {
+  function getCry() {
     $.ajax({
-      url: link+"?get="+key
+      url: link+"?get=ar_cry"
     }).done(function(data) {
-        console.log(data);
+        if(data == 1) {
+          $("#cry-pic").attr("src","./assets/img/cry1.png");
+          $("#cry-txt").html("The baby is crying.");
+        }
+        if(data == 0) {
+          $("#cry-pic").attr("src","./assets/img/cry0.png");
+          $("#cry-txt").html("Not crying.");
+        }
     }).fail(function() {
         console.error("Cannot connect to server.");
       }
     );
   };
 
-  function getIsBabyMove() {
+  function getSwing() {
     $.ajax({
-      url: link+"?get=babyMove"
+      url: link+"?get=ar_swing"
     }).done(function(data) {
-        console.log(data);
+      if(data == 1) {
+        $("#swing-pic").attr("src","./assets/img/cradle1.png");
+        $("#swing-txt").html("The cradle is swinging.");
+      }
+      if(data == 0) {
+        $("#swing-pic").attr("src","./assets/img/cradle0.png");
+        $("#swing-txt").html("Not swinging.");
+      }
     }).fail(function() {
         console.error("Cannot connect to server.");
       }
     );
   };
 
-  function getIsBabyCry() {
+  function getSound() {
     $.ajax({
-      url: link+"?get=babyCry"
+      url: link+"?get=ar_sound"
     }).done(function(data) {
-        console.log(data);
+      if(data == 1) {
+        $("#sound-pic").attr("src","./assets/img/music1.png");
+        $("#sound-txt").html("The music is playing.");
+      }
+      if(data == 0) {
+        $("#sound-pic").attr("src","./assets/img/music0.png");
+        $("#sound-txt").html("Not playing.");
+      }
     }).fail(function() {
         console.error("Cannot connect to server.");
       }
     );
   };
 
-  function getSwingCradle() {
-    $.ajax({
-      url: link+"?get=swingCradle"
-    }).done(function(data) {
-        console.log(data);
-    }).fail(function() {
-        console.error("Cannot connect to server.");
-      }
-    );
+  function get() {
+    getCry();
+    getSwing();
+    getSound();
   };
 
-  function getBuzzer() {
-    $.ajax({
-      url: link+"?get=buzzer"
-    }).done(function(data) {
-        console.log(data);
-    }).fail(function() {
-        console.error("Cannot connect to server.");
-      }
-    );
-  };
+  window.get = get;
+
+  // setInterval(get,1000);
+
 });
